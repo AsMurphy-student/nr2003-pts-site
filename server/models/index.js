c12663a000df4fcec83638f2122a56415f50b87c
AsMurphy-student <AsMurphy-work@protonmail.com>
2025-11-16 10:05:33 -0500

    Created driver model

--- server/models/index.js
+++ server/models/index.js
@@ -4,3 +4,4 @@ module.exports.Championship = require('./Championship.js');
 module.exports.Race = require('./Race.js');
 module.exports.Starts = require('./startPosition.js');
 module.exports.Finishes = require('./finishPosition.js');
+module.exports.Driver = require('./Driver.js');
