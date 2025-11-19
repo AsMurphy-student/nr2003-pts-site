5c0a00adcf9ac200871571b42d127ae664a104ad
AsMurphy-student <AsMurphy-work@protonmail.com>
2025-11-19 14:46:14 -0500

    Formatted and commented championship controller

--- server/controllers/Championship.js
+++ server/controllers/Championship.js
@@ -4,6 +4,8 @@ const updateAllDrivers = require('../helpers/drivers/updateAllDrivers');
 
 const { Championship } = models;
 
+// Renders the Championships view
+// This lists out the different championships which the user has created
 const championshipsPage = async (req, res) => {
   try {
     const query = { owner: req.session.account._id };
@@ -11,11 +13,12 @@ const championshipsPage = async (req, res) => {
 
     return res.render('championships', { championships: docs });
   } catch (err) {
-    console.log(err);
-    return res.status(500).json({ error: 'Error retrieving championships!' });
+    return res.status(500).json({ error: `Error retrieving championships: ${err}` });
   }
 };
 
+// Renders the championship overview page
+// This shows current statistics after the latest race
 const championshipOverviewPage = async (req, res) => {
   try {
     const query = { owner: req.session.account._id, name: req.params.name };
@@ -32,46 +35,54 @@ const championshipOverviewPage = async (req, res) => {
     if (!championshipData) return res.status(500).json({ error: 'Invalid championship name!' });
     return res.render('championship_overview', championshipData);
   } catch (err) {
-    return res.status(500).json({ error: 'Invalid championship name!' });
+    return res.status(500).json({ error: `Something went wrong: ${err}` });
   }
 };
 
+// This is the add race post endpoint
+// This allows the user to upload a new race file to be parsed
+// This creates both a new race file and updates/adds driver docs
 const addRace = async (req, res) => {
   try {
+    // Query championship doc that will be updated
     const query = { owner: req.session.account._id, name: req.body.name };
     const championshipToAddTo = await Championship.findOne(query)
       .select('name races drivers')
       .lean()
       .exec();
 
-    let newRaceNumber = 0;
-    if (championshipToAddTo.races) {
-      newRaceNumber = championshipToAddTo.races.length + 1;
-    } else {
-      newRaceNumber = 1;
-    }
+    // Determine new race number
+    const newRaceNumber = championshipToAddTo.races ? championshipToAddTo.races.length + 1 : 1;
+
+    // Create html string to parse
     const fileBuffer = req.files.raceFile;
     const fileString = fileBuffer.data.toString('utf8');
 
+    // Create new race doc
     const newRace = createRaceModel(fileString, newRaceNumber);
 
+    // Push new race doc to championship
     await Championship.updateOne(query, {
       $push: { races: newRace },
     });
 
+    // Create or update driver docs
     await updateAllDrivers(newRace, query);
+
+    // Redirect to overview page after updating
     return res.json({ redirect: `/championships/${championshipToAddTo.name}` });
   } catch (err) {
-    console.log(err);
-    return res.status(500).json({ error: 'Error retrieving championships!' });
+    return res.status(500).json({ error: `Error retrieving championship or parsing file: ${err}` });
   }
 };
 
+// Post request to create new championship
 const makeChampionship = async (req, res) => {
   if (!req.body.name) {
     return res.status(400).json({ error: 'Championship name is required!' });
   }
 
+  // Initialize required data
   const championshipData = {
     name: req.body.name,
     totalLaps: 0,
@@ -79,17 +90,17 @@ const makeChampionship = async (req, res) => {
   };
 
   try {
+    // Save championship and reload page
     const newChampionship = new Championship(championshipData);
     await newChampionship.save();
     return res.json({ redirect: '/championships' });
   } catch (err) {
-    console.log(err);
     if (err.code === 11000) {
       return res.status(400).json({ error: 'Championship already exists!' });
     }
     return res
       .status(500)
-      .json({ error: 'An error occured making championship!' });
+      .json({ error: `An error occured making championship: ${err}` });
   }
 };
 
